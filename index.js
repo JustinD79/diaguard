6ba57e543f14e86da75ddef625500367879c3c80
JustinD79 <justinjdunn79@gmail.com>
2025-08-10 16:55:56 -0700

    Fix window undefined error in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-if (Platform.OS === 'web') {
+if (Platform.OS === 'web' && typeof window !== 'undefined') {
   require('react-native-url-polyfill/auto');
 }
 
