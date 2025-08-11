e0fd6c089c533519ada3af2a18d3a2e44f8ce26d
JustinD79 <justinjdunn79@gmail.com>
2025-08-11 12:20:48 -0700

    Remove web-specific polyfill from index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import { Platform } from 'react-native';
-
-if (Platform.OS === 'web' && typeof window !== 'undefined') {
-  require('react-native-url-polyfill/auto');
-}
-
 import 'expo-router/entry';
