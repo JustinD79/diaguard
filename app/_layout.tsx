b83b558889f3a2768eaf86d64b3362ab6604d2ed
JustinD79 <justinjdunn79@gmail.com>
2025-09-02 11:05:15 -0700

    Fix missing SubscriptionProvider context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,23 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { AuthProvider } from '@/contexts/AuthContext';
+import { SubscriptionProvider } from '@/contexts/SubscriptionContext';
+import { ScanLimitProvider } from '@/contexts/ScanLimitContext';
 
 export default function RootLayout() {
   useFrameworkReady();
   return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
+    <AuthProvider>
+      <SubscriptionProvider>
+        <ScanLimitProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ScanLimitProvider>
+      </SubscriptionProvider>
+    </AuthProvider>
   );
 }
