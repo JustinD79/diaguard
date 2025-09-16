9aeaa711fe49b6f34ed62a593422dd1653cfd8ad
JustinD79 <justinjdunn79@gmail.com>
2025-09-16 01:52:33 -0700

    Comprehensive Metro module resolution fix

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Remove problematic serializer plugins that cause module resolution issues
+if (config.serializer && config.serializer.customSerializer) {
+  delete config.serializer.customSerializer;
+}
+
+// Ensure resolver stability
+config.resolver = {
+  ...config.resolver,
+  platforms: ['ios', 'android', 'native', 'web'],
+};
 
 module.exports = config;
