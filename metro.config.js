1df0d2ade90b20910ba86e05cd3f9b04c259d560
JustinD79 <justinjdunn79@gmail.com>
2025-09-15 21:40:56 -0700

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
-
-// Ensure resolver configuration is properly set
-config.resolver = {
-  ...config.resolver,
-  assetExts: [...(config.resolver?.assetExts || []), 'bin'],
-};
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
