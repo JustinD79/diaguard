5a36f450e3e491890a04dc986fd5f90187963ca3
JustinD79 <justinjdunn79@gmail.com>
2025-09-15 20:29:11 -0700

    Fix Expo app for device preview in Bolt

---
+++ app.config.js
@@ -0,0 +1,48 @@
+export default {
+  expo: {
+    name: "diabetes-care-app",
+    slug: "diabetes-care-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    userInterfaceStyle: "light",
+    scheme: "diabetes-care-app",
+    platforms: ["ios", "android", "web"],
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.diabetescare.app"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+      package: "com.diabetescare.app"
+    },
+    web: {
+      favicon: "./assets/images/favicon.png",
+      bundler: "metro",
+      output: "static"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera to scan food items for diabetes management."
+        }
+      ]
+    ],
+    extra: {
+      router: {
+        origin: false
+      }
+    }
+  }
+};
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,20 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// Enable web support
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Add support for additional file extensions
+config.resolver.sourceExts.push('sql', 'db');
+
+// Configure transformer for better web compatibility
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
 
 module.exports = config;
